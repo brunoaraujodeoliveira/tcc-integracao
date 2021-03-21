d37f192c4b4840d686802175285f6bba24b46149
brunoaraujodeoliveira <brunimaraujodeoliveira@gmail.com>
2021-03-21 16:33:44 -0300

    .

--- src/main/java/br/com/sgm/integracao/config/CorsMappingConfig.java
+++ src/main/java/br/com/sgm/integracao/config/CorsMappingConfig.java
@@ -1,20 +1,17 @@
 package br.com.sgm.integracao.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsMappingConfig {
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/integracao/**");
-			}
-		};
-	}
+@Configuration
+@EnableWebMvc
+public class CorsMappingConfig implements WebMvcConfigurer {
+
+	@Override
+    public void addCorsMappings(CorsRegistry registry) { 
+        registry.addMapping("/integracao/**");
+    }
 }
