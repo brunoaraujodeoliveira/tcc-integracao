3dd5ffb073a8e21853dc5fbbc25e65f9a632ed38
brunoaraujodeoliveira <brunimaraujodeoliveira@gmail.com>
2021-03-23 18:21:42 -0300

    .

--- src/main/java/br/com/sgm/integracao/config/CorsMappingConfig.java
+++ src/main/java/br/com/sgm/integracao/config/CorsMappingConfig.java
@@ -14,7 +14,7 @@ public class CorsMappingConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://tcc-gateway.herokuapp.com");
+				registry.addMapping("/**");
 			}
 		};
 	}
